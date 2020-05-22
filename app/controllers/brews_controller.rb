aacd6f7e45ef70bd5e11814af0a8c06c95e4d312
paul_vonhagen <pvonhagen26@gmail.com>
2020-05-21 20:38:12 -0700

    Built functionality and form for adding a new beer to brews.

--- app/controllers/brews_controller.rb
+++ app/controllers/brews_controller.rb
@@ -9,7 +9,30 @@ class BrewsController < ApplicationController
         end
     end
 
+    get '/brews/new' do
+        if logged_in?
+            erb :'brews/create_brew'
+        else
+            redirect to '/login'
+        end
+    end
 
+    post '/brews' do
+        if logged_in?
+            if params[:brewery] == "" || params[:beer_name] == ""
+                redirect to '/brews/new'
+            else
+                @brew = current_user.brews.build(brewery: params[:brewery], beer_name: params[:beer_name], abv: params[:abv], package_date: params[:package_date])
+                if @brew.save
+                    redirect to '/brews/#{@brew.id}'
+                else
+                    redicect to 'brews/new'
+                end
+            end
+        else
+            redirect to '/login'
+        end
+    end
 
 
 
