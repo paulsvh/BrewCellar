8c09ffe38d41a75ec7728c12d941b6107ffa49fc
paul_vonhagen <pvonhagen26@gmail.com>
2020-05-21 20:17:48 -0700

    build remaining login/logout functionality for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,10 +24,31 @@ class UsersController < ApplicationController
         end
     end
 
+    get '/login' do
+        if !logged_in?
+            erb :'users/login'
+        else
+            redirect to '/brews'
+        end
+    end
 
+    post '/login' do
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect to '/brews'
+        else
+            redirect to '/signup'
+        end
+    end
 
-
-
-
+    get '/logout' do
+        if logged_in?
+            session.destroy
+            redirect to '/login'
+        else
+            redirect to '/'
+        end
+    end
 
 end
