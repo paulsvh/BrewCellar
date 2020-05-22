c8d2f293e5baeeffc9ca119e75ea546976c03fe3
paul_vonhagen <pvonhagen26@gmail.com>
2020-05-21 20:07:20 -0700

    Built signup functionality in users_controller and create_user form page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,24 @@ class UsersController < ApplicationController
         erb :'users/show_cellar'
     end
 
-    
+    get '/signup' do
+        if !logged_in?
+            erb :'users/create_user'
+        else
+            redirect to '/brews'
+        end
+    end
+
+    post '/signup' do
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect to '/signup'
+        else
+            @user = User.new(username: params[:username]), email: params[:email], password: params[:password])
+            @user.save
+            session[:user_id] = @user.id
+            redirect to '/brews'
+        end
+    end
 
 
 
